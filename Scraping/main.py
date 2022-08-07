f49c2a2579fc4aae9cc8aafd9ae575c2e9bfe2ca
David Otero <daotdia@gmail.com>
2022-08-07 21:09:22 +0200

    Añadidos productos carrefour y mercadona

--- Scraping/main.py
+++ Scraping/main.py
@@ -3,11 +3,11 @@ import carrefour.main_carrefour as carrefourScrap
 import mercadona.main_mercadona as mercadonaScrap
 import dia.main_dia as diaScrap
 
-carrefour = carrefourScrap.Main_Carrefour()
-carrefour.updateProductosCarrefour(outFile = "Prueba1", prueba = 5)
+#carrefour = carrefourScrap.Main_Carrefour()
+#carrefour.updateProductosCarrefour(outFile = "Prueba1")
 
-mercadona = mercadonaScrap.Main_Mercadona()
-mercadona.updateProductosMercadona(outFile= "Prueba3", prueba = 5)
+#mercadona = mercadonaScrap.Main_Mercadona()
+#mercadona.updateProductosMercadona(outFile= "Prueba3")
 
 dia = diaScrap.Main_Dia()
-dia.updateProductosMercadona(outFile= "Prueba1", prueba = 5)
+dia.updateProductosMercadona(outFile= "Prueba1")
